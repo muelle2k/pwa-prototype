84fa95e15b83601ecaa191adb68c39ba8718fd9a
Patrick Mueller-klug <patrick.mueller-klug@virtual-identity.com>
2019-01-25 15:25:44 +0100

    testing code commented out

--- src/main.js
+++ src/main.js
@@ -1,9 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueWorker from 'vue-worker';
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+Vue.use(VueWorker);
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// workbox.setConfig({
+//   debug: false,
+// });
+
+// workbox.precaching.precacheAndRoute([]);
+
+// workbox.routing.registerRoute(
+//   /\.(?:js|css)$/,
+//   workbox.strategies.staleWhileRevalidate(),
+// );
+
+// workbox.routing.registerRoute(
+//   /\.(?:png|gif|jpg|jpeg|svg)$/,
+//   workbox.strategies.cacheFirst({
+//     cacheName: 'images',
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxEntries: 60,
+//         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+//       }),
+//     ],
+//   }),
+// );
